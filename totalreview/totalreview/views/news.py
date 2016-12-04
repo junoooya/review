e22cfc084b64158471ab3e668af3a00615212016
junoooya <junoooya@gmail.com>
2016-12-05 02:30:09 +0900

    refactor news.py : use settings.BASE_DIR

--- totalreview/totalreview/views/news.py
+++ totalreview/totalreview/views/news.py
@@ -1,6 +1,7 @@
 import json
 import requests
 
+from django.conf import settings
 from django.http.response import HttpResponse
 
 def news(request):
@@ -15,21 +16,19 @@ def news(request):
                     lambda news : search in news.get("title"),
                     news_list))
 
-    content = "<h1>News</h1>"+\
-            "<p>News Page</p>"+\
-            "<p>{count}개의 뉴스가 있습니다.</p>".format(
-                    count = len(news_list),
-                    )+\
-            "".join([
-                 "<h2>{title}</h2><img src={image_src}/><p>{content}<p>".format(
-                     title = news.get("title"),
-                     image_src = news.get("image"),
-                     content = news.get("content"),
-                     )
-                 for news
-                 in news_list
-                 ])
-
-
-    return HttpResponse(
-            content)
+    with open(settings.BASE_DIR + "/totalreview/templates/news.html", "r") as template:
+        content = template.read()
+        content += "<p>{count}개의 뉴스가 있습니다.</p>".format(
+                count = len(news_list),
+                )+\
+                "".join([
+                    "<h2>{title}</h2><img src={image_src}/><p>{content}<p>".format(
+                        title = news.get("title"),
+                        image_src = news.get("image"),
+                        content = news.get("content"),
+                        )
+                    for news
+                    in news_list
+                    ])
+    
+    return HttpResponse(content, )

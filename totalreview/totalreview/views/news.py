245c6fac0a1f5d7bde192a7427bf7476be984ced
junoooya <junoooya@gmail.com>
2016-12-05 02:18:36 +0900

    refactor news.py : add search

--- totalreview/totalreview/views/news.py
+++ totalreview/totalreview/views/news.py
@@ -4,11 +4,22 @@ import requests
 from django.http.response import HttpResponse
 
 def news(request):
+    search = request.GET.get("search")
+
     response = requests.get("https://watcha.net/home/news.json?page=1&per=50")
     news_dict = json.loads(response.text)
+    news_list = news_dict.get("news")
+    
+    if search:
+        news_list = list(filter(
+                    lambda news : search in news.get("title"),
+                    news_list))
 
     content = "<h1>News</h1>"+\
             "<p>News Page</p>"+\
+            "<p>{count}개의 뉴스가 있습니다.</p>".format(
+                    count = len(news_list),
+                    )+\
             "".join([
                  "<h2>{title}</h2><img src={image_src}/><p>{content}<p>".format(
                      title = news.get("title"),
@@ -16,7 +27,7 @@ def news(request):
                      content = news.get("content"),
                      )
                  for news
-                 in news_dict.get("news")
+                 in news_list
                  ])
 
 

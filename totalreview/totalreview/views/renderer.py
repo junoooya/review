79fa5b948af4c4df7e24f97897b05e199655a1b6
junoooya <junoooya@gmail.com>
2016-12-05 03:04:58 +0900

    refactor renderer.py, news.html : add header, footer

--- totalreview/totalreview/views/renderer.py
+++ totalreview/totalreview/views/renderer.py
@@ -2,8 +2,12 @@ from django.conf import settings
 from django.http.response import HttpResponse
 
 def render(template_name, context):
+    header_content = open(settings.BASE_DIR + "/totalreview/templates/header.html", "r").read()
+    footer_content = open(settings.BASE_DIR + "/totalreview/templates/footer.html", "r").read()
     with open(settings.BASE_DIR + "/totalreview/templates/" + template_name + ".html", "r") as template:
         content = template.read()
+        content = content.replace("## header ##", header_content)
+        content = content.replace("## footer ##", footer_content)
         for key, value in context.items():
             content = content.replace(
                     "## " + key + " ##",
